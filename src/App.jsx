99b938741b98fac413cb6b3d5cb61c4a9d52f72c
Sushant Singh <sushantbibhu@gmail.com>
2024-03-08 17:41:53 +0530

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+import Popular from "./Popular";
+import Story from "./Story";
+
+function App() {
+  return (
+    <div className="header">
+      <Navbar />
+      {/* <Popular /> */}
+      <Story />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpjm3MAJoEeHtjJ5cRSWyzG8-bEOy6zSA",
+  authDomain: "assignment-sysu.firebaseapp.com",
+  projectId: "assignment-sysu",
+  storageBucket: "assignment-sysu.appspot.com",
+  messagingSenderId: "857741750082",
+  appId: "1:857741750082:web:17af74f487806d0f8722db",
+};
+
+const app = initializeApp(firebaseConfig);
